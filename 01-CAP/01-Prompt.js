5518d46fba321fa40ce38793c65fb6d18f36c2c6
Jorge Suárez <suarezjorge758@gmail.com>
2023-05-31 14:37:00 -0400

    Prompt

---
+++ 01-CAP/01-Prompt.js
@@ -0,0 +1,2 @@
+let nombre = prompt("¿como quieres que te llame?"); //El prompt es un elemento como el alert() que sirve para solocitar un elemento al usuario, se puede utilizar para que el usuario te responda a lo que le pides, guardarlo en una variable y de ahí poder utilizar eso a tu favor
+
