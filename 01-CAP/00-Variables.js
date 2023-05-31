4ef8ab200ad646475afbc071e161cb09271f9db2
Jorge Suárez <suarezjorge758@gmail.com>
2023-05-31 14:14:47 -0400

    Variables

---
+++ 01-CAP/00-Variables.js
@@ -0,0 +1,18 @@
+var container = "variable";
+let container2 = "better than var";
+const container3 = "constant, cant change the value";
+
+// STRING = CADENA DE TEXTO
+// NUMBER = VALOR DE TIPO NÚMERO
+// BOOLEAN = VERDADERO O FALSO
+
+//UNDEFINED = NO DEFINIDO
+//NULL = VALOR NULO O VACÍO
+//NaN = NO ES UN NÚMERO
+
+const array = ["array1" , "array2" , "array3", 4 , 5 , 6 ]; //Elementos de un array se determinan por número desde el 0 en adelante con el length
+const object = {
+    namePerson : "Jorge",
+    agePerson : 19,
+    studyPlatform : "Platzi",
+} //Elementos de un objeto se determinan por un nombre clave, se les llama con el object.value (object.agePerson), este ya no es por posición y se separa por ,
