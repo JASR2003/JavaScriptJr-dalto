bcf9055aea837073584c858cb932355fc91633ad
Jorge Suárez <suarezjorge758@gmail.com>
2023-06-01 15:25:31 -0400

    concatenacion

---
+++ 01-CAP/02-Operadores.js
@@ -0,0 +1,14 @@
+//Asignación
+
+// = igual a
+// + sumado a
+// - restado a
+// * multiplicado por
+// / dividido entre
+// % el resto de (10 % 5 = 0, 10 % 6 = 4)
+// ** exponenciado por
+
+//Aritméticos
+
+// -- decremento
+// ++ incremento
---
+++ 01-CAP/03-Concatenacion.js
@@ -0,0 +1,6 @@
+//Unir dos strings text
+
+let saludo = "hola Jorge";
+let pregunta = "¿Como estás?";
+
+let concatenacion = saludo +" "+ pregunta;
