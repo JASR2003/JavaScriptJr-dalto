079bb898d390c7ef29bca712ed7d11f84ada6f29
Jorge Suárez <suarezjorge758@gmail.com>
2023-06-01 15:53:47 -0400

    concat

--- 01-CAP/03-Concatenacion.js
+++ 01-CAP/03-Concatenacion.js
@@ -4,3 +4,9 @@ let saludo = "hola Jorge";
 let pregunta = "¿Como estás?";
 
 let concatenacion = saludo +" "+ pregunta;
+
+// Backtiks
+
+let nombre = "Jorge";
+
+frase = `Hola ${nombre}, Bienvenido`; //al usar comillas invertidas se puede concat texto con variables usando el ${}
