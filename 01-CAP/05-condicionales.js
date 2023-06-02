71532a1aff9ef78fbcbd06884a1ca312ed7dd19f
Jorge Suárez <suarezjorge758@gmail.com>
2023-06-02 16:48:07 -0400

    if else

---
+++ 01-CAP/05-condicionales.js
@@ -0,0 +1,11 @@
+//Bloque de código que se ejecuta solo si la condición se cumple
+
+//IF, ELSE IF y ELSE, consisten en si esto es verdad se cumple la condición y así hasta que se cumpla, ejemplo
+
+if (edad < 18) {
+    console.log(`Eres menor de edad`)
+} else {
+    console.log(`Eres mayor de edad`)
+}
+
+// Si la condición que en este caso es que la edad sea menor a 18 es verdad, se cumple la primera condición, y si no se cumple ninguna condición se usa el else
