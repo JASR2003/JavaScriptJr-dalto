93a60924673019cd4a5e24ea15f575a7ac25a37e
Jorge Suárez <suarezjorge758@gmail.com>
2023-06-05 14:00:03 -0400

    correción

--- 01-CAP/06-problema.js
+++ 01-CAP/06-problema.js
@@ -74,11 +74,12 @@ if (dineroPedro >= 0.6 && dineroPedro < 1) {
 
 const deninirCompra = (numberMoney) => {
     let din = prompt(`Dinero de ${numberMoney}`);
-    if (din >= 0.6 && din < 1) return (`${numberMoney} compra helado de agua`);
-    if (din >= 1 && din < 1.6) return (`${numberMoney} compra helado de crema`);
-    if (din >= 1.6 && din < 1.7) return (`${numberMoney} compra helado de heladix`);
-    if (din >= 1.7 && din < 1.8) return (`${numberMoney} compra helado de heladovich`);
-    if (din >= 1.8 && din < 2.9) return (`${numberMoney} compra helado de helardo`);
+    if (din >= 0.6 && din < 1) return (`${numberMoney} compra helado de agua y te sobran ${numberMoney - 0.6}`);
+    if (din >= 1 && din < 1.6) return (`${numberMoney} compra helado de crema y te sobran ${numberMoney - 1}`);
+    if (din >= 1.6 && din < 1.7) return (`${numberMoney} compra helado de heladix y te sobran ${numberMoney - 1.6}`);
+    if (din >= 1.7 && din < 1.8) return (`${numberMoney} compra helado de heladovich y te sobran ${numberMoney - 1.7}`);
+    if (din >= 1.8 && din < 2.9) return (`${numberMoney} compra helado de helardo y te sobran ${numberMoney - 1.8}`);
+    if (din >= 2.9) return (`${numberMoney} compra el pote de 1/4KG o el potecito de helado con confites y te sobran ${numberMoney - 2.9}`);
     else return (`${numberMoney} no te alcanza para ninguno, trae más dinero para la próxima`);
 }
 
