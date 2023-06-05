a643f602d7a95f7b018220a0df6e85702386cd6f
Jorge Suárez <suarezjorge758@gmail.com>
2023-06-05 17:01:25 -0400

    arrays y objetos

---
+++ 02-CAP/00-arrays.js
@@ -0,0 +1,11 @@
+let array1 = ["posicion 0", "posicion 1", "posicion 2", "posicion 3", "posicion 4"] //un array son varios elementos que se guardan en una misma variable pero que se identifican por posición.
+document.write(frutas[2]); // mostraria "posicion 2" ya que en un array se cuenta desde 0
+
+//Asociativos u objetos
+
+let pc = {
+    nombre: 'jorgePC',
+    RAM: '8GB',
+    Procesador: 'Intel Core I3',
+    Almacenamiento: '1TB',
+} //acá se llama por palabras claves, ej = pc.RAM, ya no es por numeros como los arrays convencionales
