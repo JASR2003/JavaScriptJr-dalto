0ab4c12324b0e824636fad356c0308c22fc73a24
Jorge Suárez <suarezjorge758@gmail.com>
2023-06-07 16:50:21 -0400

    funciones

---
+++ 02-CAP/02-Funciones.js
@@ -0,0 +1,27 @@
+//Bloque de codigo para poder reutilizar
+
+//la forma clásica es poner la palabra function, nombre, parametros y bloque
+
+function funcionClasica(params) {
+}
+
+//Tambien se puede hacer guardandola en una variable
+
+const funcionVar = function() {
+}
+
+// y tambien se puede hacer con el metodo de flecha
+
+const funcionFlecha = () => {
+}
+
+//Return: nos devuelve un valor
+
+function saludar() {
+    alert('hola');
+    return 'elemento retornado'
+}
+
+let saludo = saludar();
+
+document.write(saludo); // va a tirar el alert pero lo que va a escribir en el document será el valor del return y el return es el ultimo elemento que muestra, si hay algo despues del return no se va a ejecutar
