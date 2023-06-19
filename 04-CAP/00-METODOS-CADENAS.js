fe096ca4e8314c9dc649b7f09be9e5248a6798cb
Jorge Suárez <suarezjorge758@gmail.com>
2023-06-19 16:04:32 -0400

    startsWidth

--- 04-CAP/00-METODOS-CADENAS.js
+++ 04-CAP/00-METODOS-CADENAS.js
@@ -1,6 +1,10 @@
 let cadena = 'cadena de prueba';
+let cadena2 = ' concatenado';
 
 //concat() -- junta dos o más cadenas y retorna una sola
 
-console.log(cadena.concat(` concatenado`)); // muestra en consola "cadena de prueba concatenado"
+console.log(cadena.concat(cadena2)); // muestra en consola "cadena de prueba concatenado"
 
+// startsWith() -- si una cadena comienza con los caracteres de la otra cadena, devuelve true, si no, devuelve un false
+
+console.log(cadena.startsWith(cadena2)); // muestra en consola "false"
