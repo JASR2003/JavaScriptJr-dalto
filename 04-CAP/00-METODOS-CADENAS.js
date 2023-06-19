f3de15e46ac10b5cc1a393ad1c57f493a2554578
Jorge Suárez <suarezjorge758@gmail.com>
2023-06-19 16:07:58 -0400

    endsWidth()

--- 04-CAP/00-METODOS-CADENAS.js
+++ 04-CAP/00-METODOS-CADENAS.js
@@ -8,3 +8,7 @@ console.log(cadena.concat(cadena2)); // muestra en consola "cadena de prueba cad
 // startsWith() -- si una cadena comienza con los caracteres de la otra cadena, devuelve true, si no, devuelve un false
 
 console.log(cadena.startsWith(cadena2)); // muestra en consola "true"
+
+// endsWith() -- si una cadena termina con los caracteres de la otra cadena, devuelve true, si no, devuelve un false
+
+console.log(cadena.endsWith(cadena2)); // muestra en consola "false"
