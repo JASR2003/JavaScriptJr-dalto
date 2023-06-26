98749d146e3a850431d5cdd7f7af7360e1cb757e
Jorge Suárez <suarezjorge758@gmail.com>
2023-06-26 10:21:01 -0400

    substring

--- 01-M-CADENAS2.js
+++ 01-M-CADENAS2.js
@@ -1,2 +1,11 @@
-let cadena = 'cadena de prueba ';
+let cadena = 'cadena de prueba';
 let cadena2 = 'cadena 2';
+
+// split() -- divide la cadena como se la pidamos en una array
+
+console.log(cadena.split(" ")); // regresa "[ 'cadena', 'de', 'prueba'
+
+// substring() -- La función substring toma dos argumentos: el índice donde comienza la subcadena y el índice donde termina la subcadena (no incluyendo este último índice). En este caso, el índice de inicio es 0 y el índice final es 4 (no incluyendo este último índice), por lo que la subcadena resultante es "Hola".
+
+const substringCadena = "Hola mundo";
+console.log(substringCadena.substring(0, 4)); // "Hola"
