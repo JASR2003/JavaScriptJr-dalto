3d6484f293f98a6ae530c209440b6e449c832dd7
Jorge Suárez <suarezjorge758@gmail.com>
2023-06-26 13:12:57 -0400

    trim

--- 01-M-CADENAS2.js
+++ 01-M-CADENAS2.js
@@ -1,5 +1,6 @@
 let cadena = 'cadena de prueba';
 let cadena2 = 'cadena 2';
+let cadenaNumber = 432;
 
 // split() -- divide la cadena como se la pidamos en una array
 
@@ -9,3 +10,14 @@ console.log(cadena.split(" ")); // regresa "[ 'cadena', 'de', 'prueba'
 
 const substringCadena = "Hola mundo";
 console.log(substringCadena.substring(0, 4)); // "Hola"
+
+console.log(cadena.toLowerCase()); // devuelve todo en minusculas
+console.log(cadena.toUpperCase()); // devuelve todo en mayusculas
+console.log(cadenaNumber.toString()); // aparentemente no hace nada diferente, sigue viendose el 432, con la diferencia de que si se le pregunta el typeOf ya no dirá number, dirá string, esto sirve cuando necesitamos que el valor obligatoriamente sea un texto y no un numero
+
+const cadenaTrim = "    Hola mundo    ";
+console.log(cadenaTrim.trim()); //quita los espacios del comienzo y del final
+console.log(cadenaTrim.trimStart()); //quita los espacios del comienzo
+console.log(cadenaTrim.trimEnd()); //quita los espacios del final
+
+
