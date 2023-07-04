115012d124ccaf1f0aad8d5234fe8104db72595d
Jorge Suárez <suarezjorge758@gmail.com>
2023-07-04 18:28:59 -0400

    segundo problema resuelto

---
+++ 04-CAP/06-problemas.js
@@ -0,0 +1,47 @@
+const obtInfo = (materia) => {
+    materiasClase = {
+        fisica: ["Perez", "pedro", "maria", "daniela", "cofla", "jorge"],
+        quimica: ["Rodriguez", "pedro","jose", "maria", "daniela"],
+        matematicas: ["Suarez", "pedro","jose", "daniela", "cofla", "jorge"],
+        programacion: ["Ramirez", "pedro","jose", "maria", "daniela", "cofla", "jorge"],
+    }
+    if(materiasClase[materia] !== undefined){
+        return [materiasClase[materia], materia, materiasClase];
+    }
+    else {
+        return materiasClase;
+    }
+}
+
+const mostrarInfo = (materia)=>{
+    let informacion = obtInfo(materia);
+
+if (informacion !== false) {
+    let profesor = obtInfo(materia)[0][0];
+    let alumnos = obtInfo(materia)[0];
+    alumnos.shift();
+    document.write(`Profesor: ${profesor} </br>
+    Materia: ${informacion[1]} </br>
+    alumnos presentes: <b>${alumnos}</b></br></br>`);
+}
+}
+
+const cantidadClases = (alumno)=>{
+    let informacion = obtInfo();
+    let cantidadTotal = 0;
+    let clasesPresentes = [];
+    for (info in informacion){
+        if(informacion[info].includes(alumno)){
+            cantidadTotal++;
+            clasesPresentes.push(" " + info);
+        }
+    }
+    return `el alumno ${alumno} está en un total de ${cantidadTotal} clases, las cuales son:<b>${clasesPresentes}</b> </br></br>`;
+}
+
+mostrarInfo("fisica");
+mostrarInfo("quimica");
+mostrarInfo("matematicas");
+mostrarInfo("programacion");
+
+document.write(cantidadClases("cofla"));
